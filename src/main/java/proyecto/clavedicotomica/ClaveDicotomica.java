865f6f2bba60ae72c7ff3ecb42ca5f666e79de92
SIMONANANIAN <simonananian2006@gmail.com>
2025-03-20 19:14:29 -0400

    Creación de estructura del proyecto de clave dicotomica

---
+++ src/main/java/proyecto/clavedicotomica/ClaveDicotomica.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package proyecto.clavedicotomica;
+
+/**
+ *
+ * @author enith
+ */
+public class ClaveDicotomica {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
